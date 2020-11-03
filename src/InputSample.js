931fd9fc715523dd2b495792353688829a42dd8c
groot <boo8672@naver.com>
2020-11-03 23:53:28 +0900

    1.1 ~ 1.10

---
+++ src/InputSample.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react';
+
+function InputSample() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+    const nameInput = useRef();
+
+    const {name, nickname} = inputs;
+
+    const onChange = (e) => {
+        const {value, name} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    }
+
+    const onReset = () => {
+        setInputs({
+            name: '',
+            nickname: '',
+        });
+        nameInput.current.focus();
+    }
+
+    return (
+        <div>
+            <input name="name" onChange={onChange} value={name} placeholder="name" ref={nameInput}/>
+            <input name="nickname" onChange={onChange} value={nickname} placeholder="nickName"/>
+            <button onClick={onReset}>초기화</button>
+            <div>
+                <b>값: </b>
+                {name} ({nickname})
+            </div>
+        </div>
+    );
+}
+
+export default InputSample;
